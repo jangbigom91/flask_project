d19f1b703465427ac901d1e3edf3b4e4dbf1d7b9
jangbigom91 <jangbigom91@gmail.com>
2021-10-14 10:04:07 +0900

    1014 final commit_2

--- practice/make.py
+++
@@ -1,4 +0,0 @@
-date = input("날짜를 입력하세요 : ")
-
-print(date)
-print(type(date))
--- practice/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import plotly.express as px
-
-df = pd.read_csv('PM10_seoul.csv')
-
-fig = px.line(df, x='Date', y='Seoul', title='PM10 of Seoul(2014~2021)')
-
-fig_seoul = fig.show()
